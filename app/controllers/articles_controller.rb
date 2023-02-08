c339d77e7010c6d300e3e8448c1a42b7dbfdefaf
takeda_ryoya <t.ryooya@gmail.com>
2023-02-08 15:11:54 +0900

    Add: 記事コントローラのCRUD機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,55 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.active.order("#{sort_column} #{sort_direction}")
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.author = current_user.id
+
+    if @article.save
+      redirect_to articles_url
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      redirect_to articles_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    ActiveRecord::Base.transaction do
+      now = Time.now
+      @article.update_column(:deleted_at, now)
+    end
+
+    redirect_to articles_url
+  end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def sort_column
     params[:sort] ? params[:sort] : 'created_at'
